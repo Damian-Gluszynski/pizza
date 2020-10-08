723867e5a77681b26a2556b90ec52a0c8c9b1738
Damian-Gluszynski <71399957+Damian-Gluszynski@users.noreply.github.com>
2020-10-08 12:46:10 -0400

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -3,4 +3,5 @@
 // Mr coxal Class grade 9
 game.splash("Pizza order Calculator")
 let Diameter = game.askForNumber("Enter Pizza Diameter")
-game.splash("")
+let Pizza_Cost = (7 * 0.5 + 0.75 + 1) * 1.13
+game.splash(convertToText(Math.round(Pizza_Cost)))
